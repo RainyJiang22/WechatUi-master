bb8f0ea78f8478ec6733e6951512273e2b96f38e
jackytallow <3434481891@qq.com>
2018-12-18 16:23:57 +0800

    编写联系人列表页面

--- Wechat-Design/app/src/main/java/com/example/wechatui_design/fragment/ContactlistFragment.java
+++ Wechat-Design/app/src/main/java/com/example/wechatui_design/fragment/ContactlistFragment.java
@@ -1,7 +1,9 @@
 package com.example.wechatui_design.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +16,23 @@ import com.example.wechatui_design.R;
 
 public class ContactlistFragment  extends Fragment{
 
+     private RecyclerView contactlist;
+    /**
+     * 先暂时存在数组里面，后期会使用数据库
+     */
+    private String[] constactsName;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contactlist,container,false);
+        View view =  inflater.inflate(R.layout.fragment_contactlist,container,false);
+        return view;
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }
