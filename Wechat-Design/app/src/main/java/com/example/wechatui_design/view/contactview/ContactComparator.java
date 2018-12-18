3a3362648479b8ea64cba6ad698a1c764e191b69
jackytallow <3434481891@qq.com>
2018-12-18 16:08:48 +0800

    将联系人列表进行排序，更新联系人列表

--- Wechat-Design/app/src/main/java/com/example/wechatui_design/view/contactview/ContactComparator.java
+++ Wechat-Design/app/src/main/java/com/example/wechatui_design/view/contactview/ContactComparator.java
@@ -9,6 +9,7 @@ import java.util.Comparator;
 public class ContactComparator  implements Comparator<String> {
 
     @Override
+    //比较
     public int compare(String o1, String o2) {
         int c1 = (o1.charAt(0) + "").toUpperCase().hashCode();
         int c2 = (o2.charAt(0) + "").toUpperCase().hashCode();
@@ -20,6 +21,8 @@ public class ContactComparator  implements Comparator<String> {
         } else if (!c1Flag && c2Flag) {
             return -1;
         }
+
+
 
         return c1 - c2;
     }
