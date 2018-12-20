e77157566584b8877fc4ec077619a5104823eb2c
jackytallow <3434481891@qq.com>
2018-12-20 19:43:09 +0800

    加载页面
    使用Thread线程以及Timer计时器

---
+++ Mychatmaster/app/src/main/java/com/example/mychat_master/Activity/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.mychat_master.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mychat_master.MainActivity;
+import com.example.mychat_master.R;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wechat_start);
+        Integer time = 2000;
+        Handler handler = new Handler();
+        //当计时结束的时候，跳转到主界面
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                SplashActivity.this.finish();
+
+            }
+        },time);
+
+    }
+}
