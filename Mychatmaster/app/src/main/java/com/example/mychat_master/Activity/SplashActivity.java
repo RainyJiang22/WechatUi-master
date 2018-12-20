c27598cd16bafd9335f5e38083f26ccfedb178b4
jackytallow <3434481891@qq.com>
2018-12-20 20:10:28 +0800

    加载微信主界面，以及四个fragment

--- Mychatmaster/app/src/main/java/com/example/mychat_master/Activity/SplashActivity.java
+++ Mychatmaster/app/src/main/java/com/example/mychat_master/Activity/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
                 SplashActivity.this.finish();
 
             }
